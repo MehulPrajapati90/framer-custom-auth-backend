9882f04be19e7298c20b57b53e9591074a9e03a4
Mehul Prajapati <ailearn90@gmail.com>
2025-08-31 16:27:33 +0530

    added keep-alive for render

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import userRouter from "./route/userRoutes.js";
 import connectDataBase from "./utils/db.js";
+import axios from "axios";
 dotenv.config();
 
 const app = express();
@@ -14,14 +15,29 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
+
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/v1/user', userRouter);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
+app.get("/keep-alive", (req, res) => {
+    res.json({
+        success: true
+    });
 })
 
-app.use('/api/v1/user', userRouter);
+const keepAlive = async () => {
+    try {
+        const { data } = await axios.get(`${process.env.BACKEND_URL}/keep-alive`);
+    } catch (e) {
+        console.log('Keep-ALive Ping Failed', e);
+    }
+}
+
+if(process.env.BACKEND_URL){
+    setInterval(keepAlive, 1000*14*60);
+}
+
 
 const connection = async () => {
     await connectDataBase();
